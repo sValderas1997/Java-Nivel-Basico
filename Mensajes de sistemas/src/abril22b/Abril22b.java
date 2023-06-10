f44a75ce953d132d04dca74f8ee79ebf304a5cbf
sValderas1997 <sebastian.valderasn@utem.cl>
2023-06-09 20:19:01 -0400

    primer repositorio

---
+++ Mensajes de sistemas/src/abril22b/Abril22b.java
@@ -0,0 +1,16 @@
+
+package abril22b;
+
+public class Abril22b 
+{
+
+    public static void main(String[] args) 
+    {
+        int datoA=-23,datoB=6;
+        
+        System.out.println("\nProgramacion \nGrafica \t2020\n");
+        System.out.println("\nDatoA=" + datoA + "\nDatoB=" + datoB);
+       
+    }
+    
+}
